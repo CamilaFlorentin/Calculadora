2da7153a761d4fab88c109b830fc785be9f4ead3
CamilaFlorentin <139394028+CamilaFlorentin@users.noreply.github.com>
2023-07-17 22:29:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-const CALCULAR = document.getElementById('calcular')
-const ERROR = document.getElementById('error')
-const FLU = document.getElementById('flu')
-const FLUJO = document.getElementById("flujo")
-const MAN = document.getElementById('man')
+const BOTON = document.getElementById("calcular");
+const ERROR = document.getElementById('error');
+const FLUJO = document.getElementById("flu");
+const INPUT = document.getElementById("peso");
+const MAN = document.getElementById('man');
 
-CALCULAR.addEventListener('click', () => {
-    const DATO = document.getElementById('peso').value
-    console.log("Dato ingresado: ", DATO)
-    if (DATO > 0 && DATO <= 30){
-        ERROR.style.display = 'none'
-        let flujo = calcFlujo(DATO)
-        let mantenimiento = flujo*1.5
-        FLU.innerHTML = flujo[0] + ' cc'
-        FLUJO.innerHTML = flujo[1] + ' cc/hr'
-        MAN.innerHTML = 'm+m/2 ' + flujo[2] + ' cc/hr'
-        FLU.style.display = 'block'
-        FLUJO.style.display = 'block'
-        MAN.style.display = 'block'
-    } else if (DATO>30){
-        let sc = superficieCorporal(DATO)
-        FLU.innerHTML = sc[0] + ' cc/hr'
-        MAN.innerHTML = sc[1] + ' cc/hr'
-        FLU.style.display = 'block'
-        FLUJO.style.display = 'none'
-        MAN.style.display = 'block'
-    }  
-     else {
-        ERROR.style.display = 'block'
-        FLU.style.display = 'none'
-        MAN.style.display = 'none'
-    }
-})
+BOTON.addEventListener("click", () => {
+    const DATO = document.getElementById("peso").value
+    if (DATO > 0 && DATO !== " "){
+        ERROR.style.display = "none"
+        if (DATO <= 30){ 
+            let flujo = calcFlujo(DATO);
+            let mantenimiento = ((flujo/24) * 1.5).toFixed(2);
+            FLUJO.innerHTML = "Cantidad total: " + flujo + " cc";
+            MAN.innerHTML = "m+m/2: " + mantenimiento + " cc/hr";
+            FLUJO.style.display = "block";
+            MAN.style.display = "block";
+        } else {
+            let flujo = supCorp(DATO);
+            let mantenimiento = ((flujo/24) * 1.5).toFixed(2);
+            FLUJO.innerHTML = "Cantidad total: " + supCorp(DATO) + " cc";
+            MAN.innerHTML = "m+m/2: " + mantenimiento + " cc/hr";
+            FLUJO.style.display = "block";
+            MAN.style.display = "block";
+        }
+    }else{
+        ERROR.style.display = 'block';
+        FLUJO.style.display = 'none';
+        MAN.style.display = 'none';
+    };    
+});
+
 function calcFlujo(peso){
-    let flujo = 0
-    if (peso>=10) {
-        flujo = peso *100
-    } else if( peso>10 && peso<=20) {
-        flujo = 1000 + ((peso-10)*50)
-    } else{
-        flujo = 1500 + ((peso-20)*20)
-    }
-    return [flujo.toFixed(2),(flujo/24).toFixed(2),((flujo/24)*1.5).toFixed(2)]
+let flujo = 0;
+if (peso <= 10){
+    flujo = peso*100;
+} else if (peso > 10 && peso <= 20){
+    flujo = (1000 + (peso - 10) * 50);
+} else if (peso > 20){
+    flujo = (1500 + (peso - 20) * 20);
+}
+    return flujo.toFixed(2);
 }
 
-function supeficieCorporal(peso){
-    let supeficieCorporal = ((peso * 4) + 7) / (peso + 90)
-    return [(supeficieCorporal * 1500).toFixed(2),(supeficieCorporal * 2000).toFixed(2)]
+function supCorp(peso){
+    let flujo = ( (peso * 4) + 7) / (peso + 90)
+    return flujo.toFixed(2);
 }
-
-
